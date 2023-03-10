3d2b76ad66652d3502b0ad039d74c17cf36d347e
Naveen <naveensridhar4@gmail.com>
2023-03-10 20:52:21 +0530

    Change the value of a to 15

--- JavaApplication2/src/javaapplication2/JavaApplication2.java
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -12,7 +12,7 @@ public class JavaApplication2 {
      */
     public static void main(String[] args) {
         // TODO code application logic here
-        int a=10;
+        int a=15;
         int b=2;
         System.out.println(a+b);
     }
