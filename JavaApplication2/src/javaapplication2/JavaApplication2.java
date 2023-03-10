c713313924c210ca050bab61a31408a91d8ffbd2
Naveen <naveensridhar4@gmail.com>
2023-03-10 20:37:26 +0530

    we Add Two variables

---
+++ JavaApplication2/src/javaapplication2/JavaApplication2.java
@@ -0,0 +1,20 @@
+
+package javaapplication2;
+
+/**
+ *
+ * @author Naveen
+ */
+public class JavaApplication2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int a=1;
+        int b=2;
+        System.out.println(a+b);
+    }
+    
+}
